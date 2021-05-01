597cecc914522686cb7b24716d199f1dc45904fd
Sahil Samantaray <sahil13399@gmail.com>
2021-05-01 12:35:30 +0530

    Sign Up functionality completed

--- src/main/java/com/nymble/demo/travel/Service/Passenger/PassengerSignUpService.java
+++ src/main/java/com/nymble/demo/travel/Service/Passenger/PassengerSignUpService.java
@@ -1,9 +1,8 @@
 package com.nymble.demo.travel.Service.Passenger;
 
-import com.nymble.demo.travel.dto.Activity;
 import com.nymble.demo.travel.dto.Passenger;
 import com.nymble.demo.travel.dto.TravelPackage;
 
 public interface PassengerSignUpService {
-    void passengerSignUp(Passenger passenger, TravelPackage travelPackage, Activity activity);
+    void passengerSignUp(Passenger passenger, TravelPackage travelPackage, String activityName);
 }
--- src/main/java/com/nymble/demo/travel/Service/Passenger/PassengerSignUpServiceImpl.java
+++ src/main/java/com/nymble/demo/travel/Service/Passenger/PassengerSignUpServiceImpl.java
@@ -1,13 +1,28 @@
 package com.nymble.demo.travel.Service.Passenger;
 
+import com.nymble.demo.travel.Exchanges.ActivityPassenger;
 import com.nymble.demo.travel.dto.Activity;
+import com.nymble.demo.travel.dto.Destination;
 import com.nymble.demo.travel.dto.Passenger;
 import com.nymble.demo.travel.dto.TravelPackage;
+
+import java.util.List;
 
 public class PassengerSignUpServiceImpl implements PassengerSignUpService{
 
     @Override
-    public void passengerSignUp(Passenger passenger, TravelPackage travelPackage, Activity activity) {
-
+    public void passengerSignUp(Passenger passenger, TravelPackage travelPackage, String activityName) {
+        List<Destination> destinationList = travelPackage.getPackageDestinations();
+        for(Destination destination : destinationList) {
+            List<Activity> activities = destination.getDestinationActivities();
+            for(Activity activity : activities) {
+                if (activityName.equals(activity.getActivityName())) {
+                    List<ActivityPassenger> activityPassengers = passenger.getActivityPassengerList();
+                    activityPassengers.add(new ActivityPassenger(destination.getDestinationName()
+                            ,activityName,0));
+                    break;
+                }
+            }
+        }
     }
 }
